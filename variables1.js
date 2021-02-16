2453786b959b37045c03c3a5369a016658e00466
Derlys Dominguez <derlys275@gmail.com>
2021-02-16 16:07:58 -0500

    feat: variables type number

---
+++ variables1.js
@@ -0,0 +1,35 @@
+// Declar una variable tipo numero
+var edad = 27;
+
+// operacion matematica sumar
+//edad = edad +1;
+edad += 1
+
+// operación matematica resta
+var peso = 65
+//peso = peso - 2
+peso -= 2
+
+var pastel = 1
+
+pesoTotal = peso + pastel
+
+var deporte = 3
+
+pesoTotal -= deporte
+
+// numeros decimales
+var preciodeVino = 200.3
+
+// Modulo de JavaScript global (Math)
+var total = Math.round(preciodeVino * 100 * 3) / 100
+
+var totalStr = total.toFixed(2)
+
+var total2 = parseFloat(totalStr)
+
+//Operación matematica división
+var pizza = 8
+var personas = 2
+
+var porciones = pizza / personas
